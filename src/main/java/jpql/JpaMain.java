025d2b78d0f3a266940a41b6487a2600256ffa24
ones1kk <ones1k95@gmail.com>
2022-05-27 22:28:33 +0900

    ✨ Add : Query API

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,5 +1,6 @@
 package jpql;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,6 +17,29 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+//            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+//            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+//
+//            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+            List<Member> result = em.createQuery("select m from Member m",
+                Member.class).getResultList();
+
+            result.forEach(it -> System.out.println("it.getUsername() : " + it.getUsername()));
+
+            Member singleResult = em.createQuery(
+                    "select m from Member m where m.username= :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+            System.out.println("singleResult.getUsername() = " + singleResult.getUsername());
+
 
         } catch (Exception e) {
             tx.rollback();
